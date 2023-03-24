df3ba7e862d0afc53c3dfd6a665ceea06bd5f188
technopremium <48184440+technopremium@users.noreply.github.com>
2023-03-24 11:24:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QPlainTextEdit
 from PyQt5.QtCore import QThread, pyqtSignal
-import trainer_code  # Assuming the provided code is saved as trainer_code.py
+import trainer  # Assuming the provided code is saved as trainer.py
 
 
 class TrainerThread(QThread):
     output = pyqtSignal(str)
 
     def run(self):
-        trainer = trainer_code.setup_trainer()
-        trainer.train()
+        trainer.setup()
+        trainer_instance = trainer.setup_trainer()
+        trainer_instance.train()
 
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("PyTorch Training GUI")
+        self.setWindowTitle("PyTorch GPU Benchmark GUI")
         self.setGeometry(100, 100, 800, 600)
 
         layout = QVBoxLayout()
@@ -24,27 +25,18 @@ class App(QMainWindow):
         self.console = QPlainTextEdit()
         self.console.setReadOnly(True)
 
-        self.install_button = QPushButton("Install")
-        self.install_button.clicked.connect(self.install)
-
-        self.start_training_button = QPushButton("Start Training")
-        self.start_training_button.clicked.connect(self.start_training)
+        self.start_benchmark_button = QPushButton("Start Benchmarking")
+        self.start_benchmark_button.clicked.connect(self.start_benchmark)
 
         layout.addWidget(self.console)
-        layout.addWidget(self.install_button)
-        layout.addWidget(self.start_training_button)
+        layout.addWidget(self.start_benchmark_button)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-    def install(self):
-        trainer_code.setup()
-        self.console.appendPlainText("Installation complete.\n")
-
-    def start_training(self):
+    def start_benchmark(self):
         self.trainer_thread = TrainerThread()
-        self.trainer_thread.output.connect(self.console.appendPlainText)
         self.trainer_thread.start()
 
     def append_output(self, text):
